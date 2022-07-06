f248b924568eb59392f6f6f586215b34a6fac1c4
michaelldix2 <97764673+michaelldix2@users.noreply.github.com>
2022-07-06 14:10:39 -0500

    test

---
+++ gridTest/WebClient/script/Defaults.js
@@ -0,0 +1,25 @@
+//*******************************************************************************/
+//*  Rocket Software makes no warranty, express or implied, with regard to      */
+//*  this material, including fitness for use. Additionally, Rocket is not      */
+//*  responsible for maintaining compatibility of this information with future  */
+//*  releases.  Rocket provides this as an example only.                        */
+//*  Customers using this information do so at their own risk.                  */
+//*  The items provided to you are identified as Type II material               */
+//*  as per the Professional Services Agreement. For the                        */
+//*  avoidance of doubt, this means that Rocket owns the IP in                  */
+//*  any such deliverables.                                                     */
+//*******************************************************************************/
+
+
+var defaults = {
+	"dynamicFont" : "dynamic-monospace",
+	"dynamicRowPadding" : 10,
+	"dynamicColumnPadding" : -1,
+	"dynamicDatePickerPadding" : 4,
+	"dynamicButtonContainerColumn" : 1,
+	"dynamicButtonContainerSpacing" : 10,
+	"dynamicPromptButtonContainerSpacing" : 4,
+	"dynamicPromptButtonAppearanceName" : "_DynamicNavbarButton",
+	"cursorKeyNavigation" : true,
+	"logDebugMessages": false
+};
